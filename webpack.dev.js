4594f69e4bf473290b5ae9b032fb2cddb7b7d8d0
DengXG <dxg0317@qq.com>
2017-09-21 11:39:01 +0800

    fix：第一章

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new Visualizer({
+      filename: './stats.html',
+    }),
   ],
   devServer: {
     contentBase: './dist',
